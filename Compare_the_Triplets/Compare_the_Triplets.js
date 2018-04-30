d0fe92f49998aeebd9322f4cee65c6aa4bdda376
adamxtokyo <adam.grawender@gmail.com>
2018-04-30 22:20:23 +0200

    https://www.hackerrank.com/challenges/compare-the-triplets/problem

---
+++ Compare_the_Triplets/Compare_the_Triplets.js
@@ -0,0 +1,19 @@
+/*
+ * Compare the Triplets
+ * https://www.hackerrank.com/challenges/compare-the-triplets/problem
+ */
+const solve = (a0, a1, a2, b0, b1, b2) => {
+    let a_score = 0;
+    let b_score = 0;
+
+    const give_points = (a, b) => {
+        if (a > b) a_score++;
+        if (a < b) b_score++;
+    }
+
+    give_points(a0, b0);
+    give_points(a1, b1);
+    give_points(a2, b2);
+
+    return [a_score, b_score];
+}
