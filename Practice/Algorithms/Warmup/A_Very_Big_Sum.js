87cc63be5846beeac32e78512d421693823ae43d
adamxtokyo <adam.grawender@gmail.com>
2018-08-21 13:55:25 +0200

    Practice > Algorithms > Warmup > A Very Big Sum

--- Practice/Algorithms/Warmup/A_Very_Big_Sum.js
+++ Practice/Algorithms/Warmup/A_Very_Big_Sum.js
@@ -4,8 +4,4 @@
  * https://www.hackerrank.com/challenges/a-very-big-sum/problem
  */
 
-function aVeryBigSum(n, ar) {
-    let sum = 0;
-    ar.forEach( (e, i) => sum += e);
-    return sum;
-}
+const aVeryBigSum = arr => arr.reduce( (acc, val) => acc + val, 0)
