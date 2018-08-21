e9554b37a4440ca0de910ca753c668a671ef6390
adamxtokyo <adam.grawender@gmail.com>
2018-08-21 14:09:07 +0200

    Practice > Algorithms > Warmup > Diagonal Difference

--- Practice/Algorithms/Warmup/Diagonal_Difference.js
+++ Practice/Algorithms/Warmup/Diagonal_Difference.js
@@ -4,12 +4,12 @@
  * https://www.hackerrank.com/challenges/diagonal-difference/problem
  */
 
-function diagonalDifference(matrix) {
-    let a = 0;
-    let b = 0;
-    for(let i = 0, l = (matrix.length - 1); i <= l; i++){
-        a += matrix[i][i];
-        b += matrix[i][l-i];
+function diagonalDifference (matrix) {
+    let a = 0
+    let b = 0
+    for (let i = 0, l = matrix.length - 1; i <= l; i++) {
+        a += matrix[i][i]
+        b += matrix[i][l-i]
     }
-    return Math.abs(a - b);
+    return Math.abs(a - b)
 }
