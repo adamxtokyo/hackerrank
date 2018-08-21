4dd7c65080c99302863d72203b05cc9986e66202
adamxtokyo <adam.grawender@gmail.com>
2018-08-21 14:05:59 +0200

    Practice > Algorithms > Warmup > Compare the Triplets

--- Practice/Algorithms/Warmup/Compare_the_Triplets.js
+++ Practice/Algorithms/Warmup/Compare_the_Triplets.js
@@ -4,18 +4,17 @@
  * https://www.hackerrank.com/challenges/compare-the-triplets/problem
  */
 
-const solve = (a0, a1, a2, b0, b1, b2) => {
-    let a_score = 0;
-    let b_score = 0;
+function compareTriplets (a, b) {
+    let result = [0, 0]
 
-    const give_points = (a, b) => {
-        if (a > b) a_score++;
-        if (a < b) b_score++;
+    for (let i = 0, len = a.length; i < len; i++) {
+        if (a[i] === b[i]) continue
+        if (a[i] > b[i]) {
+            result[0]++
+        } else {
+            result[1]++
+        }
     }
 
-    give_points(a0, b0);
-    give_points(a1, b1);
-    give_points(a2, b2);
-
-    return [a_score, b_score];
+    return result
 }
