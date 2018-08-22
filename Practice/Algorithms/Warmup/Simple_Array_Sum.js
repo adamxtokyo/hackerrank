f13e2f8c321528af02355099ccdf62c96d7e5309
adamxtokyo <adam.grawender@gmail.com>
2018-08-22 21:52:03 +0200

    Practice > Algorithms > Warmup > Simple Array Sum

---
+++ Practice/Algorithms/Warmup/Simple_Array_Sum.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Algorithms > Warmup > Simple Array Sum
+ * https://www.hackerrank.com/challenges/simple-array-sum/problem
+ */
+
+const simpleArraySum = ar => ar.reduce( (acc, val) => acc += val, 0)
