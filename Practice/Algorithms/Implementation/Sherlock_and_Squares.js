a874e5022b50e5c32ca1cd17adcabadaaee431ce
adamxtokyo <adam.grawender@gmail.com>
2018-08-30 15:21:01 +0200

    Practice > Algorithms > Implementation > Sherlock and Squares

---
+++ Practice/Algorithms/Implementation/Sherlock_and_Squares.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Algorithms > Implementation > Sherlock and Squares
+ * https://www.hackerrank.com/challenges/sherlock-and-squares/problem
+ */
+
+const squares = (a, b) => Math.floor(Math.sqrt(b)) - Math.ceil(Math.sqrt(a)) + 1
