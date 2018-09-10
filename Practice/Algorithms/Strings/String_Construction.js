6538ec13024c8356e52fca6294c0ec7214ccd847
adamxtokyo <adam.grawender@gmail.com>
2018-09-10 11:08:36 +0200

    Practice > Algorithms > Strings > String Construction

---
+++ Practice/Algorithms/Strings/String_Construction.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Algorithms > Strings > String Construction
+ * https://www.hackerrank.com/challenges/string-construction/problem
+ */
+
+const stringConstruction = s => new Set(s).size
