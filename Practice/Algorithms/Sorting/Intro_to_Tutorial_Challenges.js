e7b4685b5e26428ea8c31f8f666bd2f86bb6f2ba
adamxtokyo <adam.grawender@gmail.com>
2018-09-12 13:43:29 +0200

    Practice > Algorithms > Sorting > Intro to Tutorial Challenges

---
+++ Practice/Algorithms/Sorting/Intro_to_Tutorial_Challenges.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Algorithms > Sorting > Intro to Tutorial Challenges
+ * https://www.hackerrank.com/challenges/tutorial-intro/problem
+ */
+
+const introTutorial = (v, arr) => arr.indexOf(v)
