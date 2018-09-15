715d49171e18207794159807319b466fffc7791e
adamxtokyo <adam.grawender@gmail.com>
2018-09-15 13:05:21 +0200

    Practice > Algorithms > Search > Sherlock and Array

---
+++ Practice/Algorithms/Search/Sherlock_and_Array.js
@@ -0,0 +1,16 @@
+
+/*
+ * Practice > Algorithms > Search > Sherlock and Array
+ * https://www.hackerrank.com/challenges/sherlock-and-array/problem
+ */
+
+function balancedSums (arr) {
+    let s1 = 0
+    let s2 = arr.slice(1).reduce( (acc,v) => acc + v, 0 )
+    for (let i = 0, l = arr.length; i < l; i++) {
+        if (s1 === s2) return 'YES'
+        s2 -= arr[i+1]
+        s1 += arr[i]
+    }
+    return 'NO'
+}
