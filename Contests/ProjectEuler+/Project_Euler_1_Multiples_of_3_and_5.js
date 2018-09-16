349a325db09e7ca438154ae0852ba14556191d5a
adamxtokyo <adam.grawender@gmail.com>
2018-09-16 19:43:27 +0200

    Contests > ProjectEuler+ > Project Euler #1: Multiples of 3 and 5

--- Contests/ProjectEuler+/Project_Euler_1_Multiples_of_3_and_5.js
+++ Contests/ProjectEuler+/Project_Euler_1_Multiples_of_3_and_5.js
@@ -4,10 +4,26 @@
  * https://www.hackerrank.com/contests/projecteuler/challenges/euler001
  */
 
-const BigNumber = require('bignumber.js')
+const bn = require('bignumber.js')
+
+process.stdin.resume()
+process.stdin.setEncoding('ascii')
+
+let input_stdin = ''
+
+process.stdin.on('data', data => input_stdin += data)
+process.stdin.on('end', () => {
+    let arr = input_stdin.split('\n').map(x => new bn(x))
+    return main(arr[0], arr.slice(1))
+})
+
+function main (t, arr) {
+    for (let i = bn(0); i.lt(t); i = i.plus(1)) process.stdout.write(multiples(arr[i]) + '\n')
+}
+
+/////////////// IGNORE ABOVE THIS LINE ////////////////////
 
 function multiples (x) {
-    x = new BigNumber(x-1)
-    const sum = y => new BigNumber(y).times(x.dividedToIntegerBy(y)).times(x.dividedToIntegerBy(y).plus(1).dividedBy(2))
+    const sum = y => new bn(y).times(x.minus(1).idiv(y)).times(x.minus(1).idiv(y).plus(1).div(2))
     return sum(3).plus(sum(5)).minus(sum(15)).toFixed()
 }
