9ce08542dc3e5e0e4d90627f9eeddaf18ae7fdec
adamxtokyo <adam.grawender@gmail.com>
2018-09-18 16:32:17 +0200

    Practice > Algorithms > Greedy > Marc's Cakewalk

--- Practice/Algorithms/Greedy/Marcs_Cakewalk.js
+++ Practice/Algorithms/Greedy/Marcs_Cakewalk.js
@@ -4,7 +4,4 @@
  * https://www.hackerrank.com/challenges/marcs-cakewalk/problem
  */
 
-function marcsCakewalk (cupcakes) {
-    cupcakes.sort((a,b) => b-a)
-    return cupcakes.reduce((miles, c, i) => miles + Math.pow(2, i) * c, 0)
-}
+const marcsCakewalk = cupcakes => cupcakes.sort((a,b) => b-a).reduce((m, c, i) => m + Math.pow(2, i) * c, 0)
