242bb55c20ebcd4527da98ed4c79b6e215593a81
adamxtokyo <adam.grawender@gmail.com>
2018-09-18 16:36:05 +0200

    Practice > Algorithms > Greedy > Minimum Absolute Difference in an Array

--- Practice/Algorithms/Greedy/Minimum_Absolute_Difference_in_an_Array.js
+++ Practice/Algorithms/Greedy/Minimum_Absolute_Difference_in_an_Array.js
@@ -4,7 +4,4 @@
  * https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
  */
 
-function minimumAbsoluteDifference (arr) {
-    arr.sort((a,b) => a-b)
-    return arr.reduce((min, v, i) => Math.min(Math.abs(v - (arr[i+1] || Infinity)), min), Infinity)
-}
+const minimumAbsoluteDifference = arr => arr.sort((a,b) => a-b).reduce((min,v,i) => Math.min(Math.abs(v - (arr[i+1] || Infinity)), min), Infinity)
