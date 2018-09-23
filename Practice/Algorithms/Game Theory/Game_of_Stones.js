65403a266ae040b2d660047f3694035af7ce0168
adamxtokyo <adam.grawender@gmail.com>
2018-09-23 12:00:31 +0200

    Practice > Algorithms > Game Theory > Game of Stones

---
+++ Practice/Algorithms/Game Theory/Game_of_Stones.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Algorithms > Game Theory > Game of Stones
+ * https://www.hackerrank.com/challenges/game-of-stones-1/problem
+ */
+
+const gameOfStones = n => n % 7 > 1 ? 'First' : 'Second'
