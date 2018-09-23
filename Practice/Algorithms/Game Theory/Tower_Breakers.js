dcf6ad1fb5e2aea3553728d394e3f7cec64345d9
adamxtokyo <adam.grawender@gmail.com>
2018-09-23 12:08:37 +0200

    Practice > Algorithms > Game Theory > Tower Breakers

---
+++ Practice/Algorithms/Game Theory/Tower_Breakers.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Algorithms > Game Theory > Tower Breakers
+ * https://www.hackerrank.com/challenges/tower-breakers-1/
+ */
+
+const towerBreakers = (n, m) => m < 2 || n % 2 < 1 ? 2 : 1
