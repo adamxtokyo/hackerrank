083d761e0ff562c3cf73cac2c8af97f5d1270ada
adamxtokyo <adam.grawender@gmail.com>
2018-09-24 14:12:37 +0200

    Practice > SQL > Basic Select > Weather Observation Station 1

---
+++ Practice/SQL/Basic Select/Weather_Observation_Station_1.sql
@@ -0,0 +1,8 @@
+
+/*
+ * Practice > SQL > Basic Select > Weather Observation Station 1
+ * https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+ */
+
+SELECT CITY, STATE
+FROM STATION
