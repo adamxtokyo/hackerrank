e0572f85e75a468c3b8482fd6f490c64697282ab
adamxtokyo <adam.grawender@gmail.com>
2018-09-24 13:55:43 +0200

    Practice > SQL > Basic Select > Japanese Cities' Names

---
+++ Practice/SQL/Basic Select/Japanese_Cities_Names.sql
@@ -0,0 +1,9 @@
+
+/*
+ * Practice > SQL > Basic Select > Japanese Cities' Names
+ * https://www.hackerrank.com/challenges/japanese-cities-name/problem
+ */
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'JPN'
