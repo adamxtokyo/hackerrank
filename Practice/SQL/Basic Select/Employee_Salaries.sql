50217887ecd041f61fcf3c78e1aa1884083dac9a
adamxtokyo <adam.grawender@gmail.com>
2018-09-24 14:26:22 +0200

    Practice > SQL > Basic Select > Employee Salaries

---
+++ Practice/SQL/Basic Select/Employee_Salaries.sql
@@ -0,0 +1,12 @@
+
+/*
+ * Practice > SQL > Basic Select > Employee Salaries
+ * https://www.hackerrank.com/challenges/salary-of-employees/problem
+ */
+
+SELECT name
+FROM employee
+WHERE
+    salary > 2000 AND
+    months < 10
+ORDER BY employee_id ASC
