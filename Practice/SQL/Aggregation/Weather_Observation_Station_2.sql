e2f8d51440df6b4cb908a7ec9b8f2bf610de7d5c
adamxtokyo <adam.grawender@gmail.com>
2018-09-24 14:07:02 +0200

    Practice > SQL > Aggregation > Weather Observation Station 2

---
+++ Practice/SQL/Aggregation/Weather_Observation_Station_2.sql
@@ -0,0 +1,8 @@
+
+/*
+ * Practice > SQL > Aggregation > Weather Observation Station 2
+ * https://www.hackerrank.com/challenges/weather-observation-station-2/problem
+ */
+
+SELECT ROUND(SUM(LAT_N), 2), ROUND(SUM(LONG_W), 2)
+FROM STATION
