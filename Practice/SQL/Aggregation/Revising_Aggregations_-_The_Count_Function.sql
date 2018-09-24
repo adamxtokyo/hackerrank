987245cf29552b488ca7f4f54c3fd9a256225146
adamxtokyo <adam.grawender@gmail.com>
2018-09-24 21:50:44 +0200

    Practice > SQL > Aggregation > Revising Aggregations - The Count Function

---
+++ Practice/SQL/Aggregation/Revising_Aggregations_-_The_Count_Function.sql
@@ -0,0 +1,9 @@
+
+/*
+ * Practice > SQL > Aggregation > Revising Aggregations - The Count Function
+ * https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+ */
+
+SELECT COUNT(*)
+FROM City
+WHERE Population > 100000
