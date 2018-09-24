ecdaf4c950b265805e1ae8a349629208434aed3f
adamxtokyo <adam.grawender@gmail.com>
2018-09-24 13:47:11 +0200

    Practice > SQL >Basic Select > Revising the Select Query II

---
+++ Practice/SQL/Basic Select/Revising_the_Select_Query_II.sql
@@ -0,0 +1,11 @@
+
+/*
+ * Practice > SQL >Basic Select > Revising the Select Query II
+ * https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+ */
+
+SELECT NAME
+FROM CITY
+WHERE
+    COUNTRYCODE = 'USA' AND
+    POPULATION > 120000
