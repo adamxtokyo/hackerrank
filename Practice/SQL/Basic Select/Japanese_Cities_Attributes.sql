3034f3ef3dcb1a3af215f8722fd35500b504e9cd
adamxtokyo <adam.grawender@gmail.com>
2018-09-24 13:53:46 +0200

    Practice > SQL > Basic Select > Japanese Cities' Attributes

---
+++ Practice/SQL/Basic Select/Japanese_Cities_Attributes.sql
@@ -0,0 +1,9 @@
+
+/*
+ * Practice > SQL > Basic Select > Japanese Cities' Attributes
+ * https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+ */
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'JPN'
