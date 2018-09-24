0567943c2b475ad1d0b469ed311a13ad7b2d91ad
adamxtokyo <adam.grawender@gmail.com>
2018-09-24 22:07:24 +0200

    Practice > SQL > Aggregation > Population Density Difference

---
+++ Practice/SQL/Aggregation/Population_Density_Difference.sql
@@ -0,0 +1,8 @@
+
+/*
+ * Practice > SQL > Aggregation > Population Density Difference
+ * https://www.hackerrank.com/challenges/population-density-difference/problem
+ */
+
+SELECT MAX(Population) - MIN(Population)
+FROM City
