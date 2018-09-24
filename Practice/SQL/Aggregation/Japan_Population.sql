3e25b43a51d1beb3ac5c6a50f4c63ec423fd1f44
adamxtokyo <adam.grawender@gmail.com>
2018-09-24 22:05:56 +0200

    Practice > SQL > Aggregation > Japan Population

---
+++ Practice/SQL/Aggregation/Japan_Population.sql
@@ -0,0 +1,9 @@
+
+/*
+ * Practice > SQL > Aggregation > Japan Population
+ * https://www.hackerrank.com/challenges/japan-population/problem
+ */
+
+SELECT SUM(Population)
+FROM City
+WHERE CountryCode = 'JPN'
