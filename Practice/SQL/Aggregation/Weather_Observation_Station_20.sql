571dcd89ffacb5f55b5bb4ae3fdb09fd18862365
adamxtokyo <adam.grawender@gmail.com>
2018-09-25 12:20:26 +0200

    Practice > SQL > Aggregation > Weather Observation Station 20

---
+++ Practice/SQL/Aggregation/Weather_Observation_Station_20.sql
@@ -0,0 +1,13 @@
+
+/*
+ * Practice > SQL > Aggregation > Weather Observation Station 20
+ * https://www.hackerrank.com/challenges/weather-observation-station-20/problem
+ */
+
+SET @r = 0;
+
+SELECT ROUND(AVG(Lat_N), 4)
+FROM (SELECT (@r := @r + 1) AS r, Lat_N FROM Station ORDER BY Lat_N) Temp
+WHERE
+    r = (SELECT CEIL(COUNT(*) / 2) FROM Station) OR
+    r = (SELECT FLOOR((COUNT(*) / 2) + 1) FROM Station)
