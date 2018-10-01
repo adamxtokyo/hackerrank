01d5d81a21b9597089d1ab23f2011885a72133e5
adamxtokyo <adam.grawender@gmail.com>
2018-10-01 17:25:12 +0200

    Practice > Interview Preparation Kit > String Manipulation > Sherlock and the Valid String

---
+++ Practice/Interview Preparation Kit/String Manipulation/Sherlock_and_the_Valid_String.js
@@ -0,0 +1,26 @@
+
+/*
+ * Practice > Interview Preparation Kit > String Manipulation > Sherlock and the Valid String
+ * https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem
+ */
+
+function isValid (s) {
+
+    // Create frequency index
+    let id = new Array(26).fill(0)
+    s.split('').forEach((v,i) => id[s.charCodeAt(i) - 97]++)
+
+    // Create reverse frequency index
+    let rid = new Array(Math.max(...id) + 1).fill(0)
+    id.forEach((v,i) => rid[id[i]]++)
+
+    // Figure out the most common frequency
+    let mc = rid.indexOf(Math.max(...rid.slice(1)))
+
+    // Figure out how many unique frequencies there are
+    let l = rid.slice(1).filter(x => x > 0).length
+
+    if (l === 1) return 'YES'
+    if (l === 2 && (rid[1] === 1 || rid[mc+1] === 1)) return 'YES'
+    return 'NO'
+}
