3dbe62a9b3b7c87363952c42d175f0c14298aca9
adamxtokyo <adam.grawender@gmail.com>
2018-10-06 14:20:29 +0200

    Practice > Interview Preparation Kit > Greedy Algorithms > Minimum Absolute Difference in an Array

--- Practice/Interview Preparation Kit/Greedy Algorithms/Minimum_Absolute_Difference_in_an_Array.js
+++ Practice/Interview Preparation Kit/Greedy Algorithms/Minimum_Absolute_Difference_in_an_Array.js
@@ -4,6 +4,23 @@
  * https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
  */
 
+'use strict'
+
+process.stdin.resume()
+process.stdin.setEncoding('ascii')
+
+let input_stdin = ''
+
+process.stdin.on('data', data => input_stdin += data)
+process.stdin.on('end', () => {
+    let input =  input_stdin.split('\n')
+    // let n = Number(input[0])
+    let arr = input[1].split(' ').map(Number)
+    process.stdout.write(minimumAbsoluteDifference(arr) + '\n')
+})
+
+// --- SOLUTION BELOW --- //
+
 function minimumAbsoluteDifference (arr) {
     arr.sort((a,b) => a-b)
     return arr.reduce( (min_diff,v,i) => {
