4f2d3f88a6a56a1ed817dcaf01076f18e1e4ce69
adamxtokyo <adam.grawender@gmail.com>
2018-10-06 14:24:08 +0200

    Practice > Interview Preparation Kit > String Manipulation > Alternating Characters

--- Practice/Interview Preparation Kit/String Manipulation/Alternating_Characters.js
+++ Practice/Interview Preparation Kit/String Manipulation/Alternating_Characters.js
@@ -4,4 +4,24 @@
  * https://www.hackerrank.com/challenges/alternating-characters/problem
  */
 
-const alternatingCharacters = s => [...s].reduce( (a,v,i) => a += v === s[i+1], 0 )
+'use strict'
+
+process.stdin.resume()
+process.stdin.setEncoding('ascii')
+
+let input_stdin = ''
+
+process.stdin.on('data', data => input_stdin += data)
+process.stdin.on('end', () => {
+    let input =  input_stdin.split('\n')
+    for (let i = 1, q = Number(input[0]); i <= q; i++) {
+        let s = input[i]
+        process.stdout.write(alternatingCharacters(s) + '\n')
+    }
+})
+
+// --- SOLUTION BELOW --- //
+
+function alternatingCharacters (s) {
+    return [...s].reduce((a,v,i) => a += (v === s[i+1]), 0)
+}
