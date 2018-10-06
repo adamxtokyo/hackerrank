ae3a77b189250c908e8838708f5f16a4938608c0
adamxtokyo <adam.grawender@gmail.com>
2018-10-06 14:28:09 +0200

    Practice > Interview Preparation Kit > String Manipulation > Strings: Making Anagrams

--- Practice/Interview Preparation Kit/String Manipulation/Strings_Making_Anagrams.js
+++ Practice/Interview Preparation Kit/String Manipulation/Strings_Making_Anagrams.js
@@ -4,6 +4,21 @@
  * https://www.hackerrank.com/challenges/ctci-making-anagrams/problem
  */
 
+'use strict'
+
+process.stdin.resume()
+process.stdin.setEncoding('ascii')
+
+let input_stdin = ''
+
+process.stdin.on('data', data => input_stdin += data)
+process.stdin.on('end', () => {
+    let [a, b] =  input_stdin.split('\n')
+    process.stdout.write(makeAnagram(a, b) + '\n')
+})
+
+// --- SOLUTION BELOW --- //
+
 function makeAnagram (a, b) {
     let index = new Array(27).fill(0)
     for (let i = 0, l = Math.max(a.length, b.length); i < l; i++) {
