9c07bcd5a6d3afa3ea5fb062ab2ed731c9965813
adamxtokyo <adam.grawender@gmail.com>
2018-10-06 13:30:52 +0200

    Practice > Interview Preparation Kit > Arrays > Arrays: Left Rotation

--- Practice/Interview Preparation Kit/Arrays/Arrays_Left_Rotation.js
+++ Practice/Interview Preparation Kit/Arrays/Arrays_Left_Rotation.js
@@ -4,4 +4,23 @@
  * https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem
  */
 
-const rotLeft = (a, d) => [...a.slice(d), ...a.slice(0, d)]
+'use strict'
+
+process.stdin.resume()
+process.stdin.setEncoding('ascii')
+
+let input_stdin = ''
+
+process.stdin.on('data', data => input_stdin += data)
+process.stdin.on('end', () => {
+    let input =  input_stdin.split('\n')
+    let [n, d] = input[0].split(' ').map(Number)
+    let a = input[1].split(' ').map(Number)
+    process.stdout.write(rotLeft(a, d).join(' ') + '\n')
+})
+
+// --- SOLUTION BELOW --- //
+
+function rotLeft (a, d) {
+    return [...a.slice(d), ...a.slice(0, d)]
+}
