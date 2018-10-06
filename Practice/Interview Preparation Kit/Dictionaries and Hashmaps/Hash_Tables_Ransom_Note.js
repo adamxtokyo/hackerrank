a7a62968135c6cd32fd5a38d39c839c8b4113b80
adamxtokyo <adam.grawender@gmail.com>
2018-10-06 13:43:03 +0200

    Practice > Interview Preparation Kit > Dictionaries and Hashmaps > Hash Tables: Ransom Note

--- Practice/Interview Preparation Kit/Dictionaries and Hashmaps/Hash_Tables_Ransom_Note.js
+++ Practice/Interview Preparation Kit/Dictionaries and Hashmaps/Hash_Tables_Ransom_Note.js
@@ -4,6 +4,24 @@
  * https://www.hackerrank.com/challenges/ctci-ransom-note/problem
  */
 
+'use strict'
+
+process.stdin.resume()
+process.stdin.setEncoding('ascii')
+
+let input_stdin = ''
+
+process.stdin.on('data', data => input_stdin += data)
+process.stdin.on('end', () => {
+    let input =  input_stdin.split('\n')
+    // let [m, n] = input[0].split(' ').map(Number)
+    let magazine = input[1].split(' ')
+    let note = input[2].split(' ')
+    process.stdout.write(checkMagazine(magazine, note) + '\n')
+})
+
+// --- SOLUTION BELOW --- //
+
 function checkMagazine (magazine, note) {
 
     let word_map = new Map()
