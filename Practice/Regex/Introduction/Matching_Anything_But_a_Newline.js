e188e29e14366deea4e67e572a95fb8edc8433fd
adamxtokyo <adam.grawender@gmail.com>
2018-10-11 11:02:34 +0200

    Practice > Regex > Introduction > Matching Anything But a Newline

---
+++ Practice/Regex/Introduction/Matching_Anything_But_a_Newline.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Introduction > Matching Anything But a Newline
+ * https://www.hackerrank.com/challenges/matching-anything-but-new-line/problem
+ */
+
+let Regex_Pattern = /^.{3}\..{3}\..{3}\..{3}$/g
