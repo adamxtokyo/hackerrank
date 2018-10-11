731331b90463257f82128ba9d9e75f1aca17c694
adamxtokyo <adam.grawender@gmail.com>
2018-10-11 11:05:51 +0200

    Practice > Regex > Introduction > Matching Digits & Non-Digit Characters

---
+++ Practice/Regex/Introduction/Matching_Digits_&_Non-Digit_Characters.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Introduction > Matching Digits & Non-Digit Characters
+ * https://www.hackerrank.com/challenges/matching-digits-non-digit-character/problem
+ */
+
+let Regex_Pattern = /\d{2}\D\d{2}\D\d{4}/
