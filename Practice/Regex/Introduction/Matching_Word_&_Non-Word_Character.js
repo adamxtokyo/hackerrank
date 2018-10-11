5168521a2a401e0dd5cc904f71d65ec788a36222
adamxtokyo <adam.grawender@gmail.com>
2018-10-11 11:16:50 +0200

    Practice > Regex > Introduction > Matching Word & Non-Word Character

---
+++ Practice/Regex/Introduction/Matching_Word_&_Non-Word_Character.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Introduction > Matching Word & Non-Word Character
+ * https://www.hackerrank.com/challenges/matching-word-non-word/problem
+ */
+
+let Regex_Pattern = /\w{3}\W\w{10}\W\w{3}/
