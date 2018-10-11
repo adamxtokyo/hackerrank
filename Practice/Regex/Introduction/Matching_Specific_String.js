be2b523dfda3c0eab27fa00229066a019ee6fbcd
adamxtokyo <adam.grawender@gmail.com>
2018-10-11 10:56:24 +0200

    Practice > Regex > Introduction > Matching Specific String

---
+++ Practice/Regex/Introduction/Matching_Specific_String.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Introduction > Matching Specific String
+ * https://www.hackerrank.com/challenges/matching-specific-string/problem
+ */
+
+let Regex_Pattern = /hackerrank/
