ba2f02acaf1ea70efbc1ffaefcd3fcd3ac29357c
adamxtokyo <adam.grawender@gmail.com>
2018-10-11 11:13:51 +0200

    Practice > Regex > Introduction > Matching Whitespace & Non-Whitespace Character

---
+++ Practice/Regex/Introduction/Matching_Whitespace_&_Non-Whitespace_Character.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Introduction > Matching Whitespace & Non-Whitespace Character
+ * https://www.hackerrank.com/challenges/matching-whitespace-non-whitespace-character/problem
+ */
+
+let Regex_Pattern = /\S{2}\s\S{2}\s\S{2}/
