1be410531a6cf825883ba3f8e81ecca2d0d7f0b5
adamxtokyo <adam.grawender@gmail.com>
2018-10-13 11:24:44 +0200

    Practice > Regex > Character Class > Matching Specific Characters

---
+++ Practice/Regex/Character Class/Matching_Specific_Characters.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Character Class > Matching Specific Characters
+ * https://www.hackerrank.com/challenges/matching-specific-characters/problem
+ */
+
+let Regex_Pattern = /^[123][120][xs0][30Aa][xsu][.,]$/
