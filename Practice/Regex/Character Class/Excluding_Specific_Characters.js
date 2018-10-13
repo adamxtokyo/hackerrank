dad13bf800316f79f2075413fbe2befca216fb94
adamxtokyo <adam.grawender@gmail.com>
2018-10-13 11:29:04 +0200

    Practice > Regex > Character Class > Excluding Specific Characters

---
+++ Practice/Regex/Character Class/Excluding_Specific_Characters.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Character Class > Excluding Specific Characters
+ * https://www.hackerrank.com/challenges/excluding-specific-characters/problem
+ */
+
+let Regex_Pattern = /^\D[^aeiou][^bcDF]\S[^AEIOU][^.,]$/
