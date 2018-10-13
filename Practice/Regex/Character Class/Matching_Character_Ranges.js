dc28d85fe5eef038a5dab44401f08a5abfffea0a
adamxtokyo <adam.grawender@gmail.com>
2018-10-13 11:32:53 +0200

    Practice > Regex > Character Class > Matching Character Ranges

---
+++ Practice/Regex/Character Class/Matching_Character_Ranges.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Character Class > Matching Character Ranges
+ * https://www.hackerrank.com/challenges/matching-range-of-characters/problem
+ */
+
+let Regex_Pattern = /^[a-z][1-9][^a-z][^A-Z][A-Z]/
