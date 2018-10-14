70340f38adf205a6a939b0894b1fef7ba9bf28ba
adamxtokyo <adam.grawender@gmail.com>
2018-10-14 11:00:24 +0200

    Practice > Regex > Repetitions > Matching One Or More Repetitions

---
+++ Practice/Regex/Repetitions/Matching_One_Or_More_Repetitions.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Repetitions > Matching One Or More Repetitions
+ * https://www.hackerrank.com/challenges/matching-one-or-more-repititions/problem
+ */
+
+let Regex_Pattern = /^\d+[A-Z]+[a-z]+$/
