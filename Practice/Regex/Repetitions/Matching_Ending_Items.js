53639130d44550b58224f954c27c72e7d5e94c16
adamxtokyo <adam.grawender@gmail.com>
2018-10-14 11:03:17 +0200

    Practice > Regex > Repetitions > Matching Ending Items

---
+++ Practice/Regex/Repetitions/Matching_Ending_Items.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Repetitions > Matching Ending Items
+ * https://www.hackerrank.com/challenges/matching-ending-items/problem
+ */
+
+let Regex_Pattern = /^[a-zA-Z]*[s]{1}$/
