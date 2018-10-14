f72dc9e60db8ff8e951275db528b04b55e906439
adamxtokyo <adam.grawender@gmail.com>
2018-10-14 10:39:15 +0200

    Practice > Regex > Repetitions > Matching {x} Repetitions

---
+++ Practice/Regex/Repetitions/Matching_x_Repetitions.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Repetitions > Matching {x} Repetitions
+ * https://www.hackerrank.com/challenges/matching-x-repetitions/problem
+ */
+
+let Regex_Pattern = /^[A-Za-z02468]{40}[13579\s]{5}$/
