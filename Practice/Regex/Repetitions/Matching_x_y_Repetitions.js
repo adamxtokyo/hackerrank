6b381dfc047aaa928d49fa370e1b97f023f7b5d7
adamxtokyo <adam.grawender@gmail.com>
2018-10-14 10:54:40 +0200

    Practice > Regex > Repetitions > Matching {x, y} Repetitions

---
+++ Practice/Regex/Repetitions/Matching_x_y_Repetitions.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Repetitions > Matching {x, y} Repetitions
+ * https://www.hackerrank.com/challenges/matching-x-y-repetitions/problem
+ */
+
+let Regex_Pattern = /^\d{1,2}[a-zA-z]{3,}[\.]{0,3}$/
