b8b4aee359875c3dca40bbc32d087c7dcf6611db
adamxtokyo <adam.grawender@gmail.com>
2018-10-14 10:57:09 +0200

    Practice > Regex > Repetitions > Matching Zero Or More Repetitions

---
+++ Practice/Regex/Repetitions/Matching_Zero_Or_More_Repetitions.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Repetitions > Matching Zero Or More Repetitions
+ * https://www.hackerrank.com/challenges/matching-zero-or-more-repetitions/problem
+ */
+
+let Regex_Pattern = /^\d{2,}[a-z]*[A-Z]*$/
