dcd010ada2bfa74027969d8e10ef61b85fcfe523
adamxtokyo <adam.grawender@gmail.com>
2018-10-15 10:41:41 +0200

    Practice > Regex > Grouping and Capturing > Alternative Matching

---
+++ Practice/Regex/Grouping and Capturing/Alternative_Matching.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Grouping and Capturing > Alternative Matching
+ * https://www.hackerrank.com/challenges/alternative-matching/problem
+ */
+
+let Regex_Pattern = /^(Mr|Mrs|Ms|Dr|Er)\.[a-zA-Z]+$/
