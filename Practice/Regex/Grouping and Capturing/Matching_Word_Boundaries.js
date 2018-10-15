e0d5d1b5b7dd3c8626491c17321482189127a1a2
adamxtokyo <adam.grawender@gmail.com>
2018-10-15 10:32:18 +0200

    Practice > Regex > Grouping and Capturing > Matching Word Boundaries

---
+++ Practice/Regex/Grouping and Capturing/Matching_Word_Boundaries.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Grouping and Capturing > Matching Word Boundaries
+ * https://www.hackerrank.com/challenges/matching-word-boundaries/problem
+ */
+
+let Regex_Pattern = /\b[aeiou]+[a-z]*\b/i
