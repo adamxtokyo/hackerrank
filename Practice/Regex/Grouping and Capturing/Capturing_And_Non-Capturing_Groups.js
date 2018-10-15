2bc8f98afe30aafbb958f983102569a77391c45c
adamxtokyo <adam.grawender@gmail.com>
2018-10-15 10:35:36 +0200

    Practice > Regex > Grouping and Capturing > Capturing & Non-Capturing Groups

---
+++ Practice/Regex/Grouping and Capturing/Capturing_And_Non-Capturing_Groups.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Grouping and Capturing > Capturing & Non-Capturing Groups
+ * https://www.hackerrank.com/challenges/capturing-non-capturing-groups/problem
+ */
+
+let Regex_Pattern = /(ok){3,}/
