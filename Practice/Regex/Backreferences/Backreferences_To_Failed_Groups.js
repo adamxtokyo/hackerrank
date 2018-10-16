703342848b321f61c79c86997baddc5beb103997
adamxtokyo <adam.grawender@gmail.com>
2018-10-16 11:29:38 +0200

    Practice > Regex > Backreferences > Backreferences To Failed Groups

---
+++ Practice/Regex/Backreferences/Backreferences_To_Failed_Groups.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Backreferences > Backreferences To Failed Groups
+ * https://www.hackerrank.com/challenges/backreferences-to-failed-groups/problem
+ */
+
+let Regex_Pattern = /^\d{2}(-?)\d{2}\1\d{2}\1\d{2}$/
