f569207ac0ac80a0ff81aaa66060a57c6920ccab
adamxtokyo <adam.grawender@gmail.com>
2018-10-16 12:05:01 +0200

    Practice > Regex > Backreferences > Forward References

---
+++ Practice/Regex/Backreferences/Forward_References.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Practice > Regex > Backreferences > Forward References
+ * https://www.hackerrank.com/challenges/forward-references/problem
+ */
+
+$Regex_Pattern = '/^(\2tactic|(tac))(tac|tactic)+$/';
+
+?>
