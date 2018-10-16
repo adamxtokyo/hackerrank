fa50ddffa968e3b82328279d6df8084f01bfbb51
adamxtokyo <adam.grawender@gmail.com>
2018-10-16 11:14:56 +0200

    Practice > Regex > Backreferences > Matching Same Text Again & Again

---
+++ Practice/Regex/Backreferences/Matching_Same_Text_Again_And_Again.js
@@ -0,0 +1,7 @@
+
+/*
+ * Practice > Regex > Backreferences > Matching Same Text Again & Again
+ * https://www.hackerrank.com/challenges/matching-same-text-again-again/problem
+ */
+
+let Regex_Pattern = /^([a-z])(\w)(\s)(\W)(\d)(\D)([A-Z])([a-zA-Z])([aeiouAEIOU])(\S)\1\2\3\4\5\6\7\8\9\10$/
