7d4938cf05c1315c6d5f81aee6bc3aa0ab36bac2
adamxtokyo <adam.grawender@gmail.com>
2018-10-16 11:49:29 +0200

    Practice > Regex > Backreferences > Branch Reset Groups

---
+++ Practice/Regex/Backreferences/Branch_Reset_Groups.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Practice > Regex > Backreferences > Branch Reset Groups
+ * https://www.hackerrank.com/challenges/branch-reset-groups/problem
+ */
+
+$Regex_Pattern = '/^\d{2}(---|-|\.|:)\d{2}\1\d{2}\1\d{2}$/';
+
+?>
