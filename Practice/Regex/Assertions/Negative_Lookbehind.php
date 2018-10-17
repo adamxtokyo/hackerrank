c73c1f2936d417a627551492325ae3c165d49c60
adamxtokyo <adam.grawender@gmail.com>
2018-10-17 12:30:52 +0200

    Practice > Regex > Assertions > Negative Lookbehind

---
+++ Practice/Regex/Assertions/Negative_Lookbehind.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Practice > Regex > Assertions > Negative Lookbehind
+ * https://www.hackerrank.com/challenges/negative-lookbehind/problem
+ */
+
+$Regex_Pattern = '/(?<![aeiouAEIOU])./';
+
+?>
