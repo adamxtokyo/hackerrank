1f919a052af1adb269333b8c9277fba4fe800563
adamxtokyo <adam.grawender@gmail.com>
2018-10-17 12:26:46 +0200

    Practice > Regex > Assertions > Positive Lookbehind

---
+++ Practice/Regex/Assertions/Positive_Lookbehind.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * Practice > Regex > Assertions > Positive Lookbehind
+ * https://www.hackerrank.com/challenges/positive-lookbehind/problem
+ */
+
+$Regex_Pattern = '/(?<=[13579])\d/';
+
+?>
