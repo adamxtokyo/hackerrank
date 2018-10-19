5c993307a5bdfa70dc07a837d43082482bc260c6
adamxtokyo <adam.grawender@gmail.com>
2018-10-19 18:46:05 +0200

    Practice > Regex > Applications > UK and US: Part 2

---
+++ Practice/Regex/Applications/UK_and_US_Part_2.js
@@ -0,0 +1,36 @@
+
+/*
+ * Practice > Regex > Applications > UK and US: Part 2
+ * https://www.hackerrank.com/challenges/uk-and-us-2/problem
+ */
+
+'use strict'
+
+process.stdin.resume()
+process.stdin.setEncoding('ascii')
+
+let input_stdin = ''
+
+process.stdin.on('data', data => input_stdin += data)
+process.stdin.on('end', () => {
+    const input =  input_stdin.split('\n')
+
+    const n = Number(input[0])
+    const text = input.slice(1, 1+n).join('\n')
+
+    const t = Number(input[1+n])
+    const tests = input.slice(2+n, 2+n+t)
+
+    const result = wordCount(text, tests)
+    process.stdout.write(result.join('\n'))
+})
+
+// --- SOLUTION BELOW --- //
+
+function wordCount (text, tests) {
+    return tests.map(t => {
+        const uk = t
+        const us = uk.replace(/our/, 'or')
+        return (text.match(new RegExp(`(?:${us}|${uk})\\b`, 'g')) || []).length
+    })
+}
