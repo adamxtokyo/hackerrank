b2f30b8542fea367dd25d1a55137d047b6072c24
adamxtokyo <adam.grawender@gmail.com>
2018-10-28 12:13:36 +0100

    Practice > Tutorials > 10 Days of Statistics > Day 0: Mean, Median, and Mode
    Moved sorting outside of function and renamed variable

--- Practice/Tutorials/10_Days_of_Statistics/Day_0_Mean_Median_and_Mode.js
+++ Practice/Tutorials/10_Days_of_Statistics/Day_0_Mean_Median_and_Mode.js
@@ -17,7 +17,7 @@ process.stdin.on('end', () => {
     const input = stdin.split('\n')
 
     const n = Number(input[0])
-    const arr = input[1].split(' ').map(Number)
+    const arr = input[1].split(' ').map(Number).sort((a, b) => a - b)
 
     const result = calcMeanMedianMode(arr, n)
     process.stdout.write(result.map(x => x.toFixed(1)).join('\n'))
@@ -26,8 +26,7 @@ process.stdin.on('end', () => {
 // --- SOLUTION BELOW --- //
 
 function calcMeanMedianMode (arr, n = arr.length) {
-    arr.sort((a, b) => a - b)
-    const count = arr.reduce((acc, val) => {
+    const frequencyMap = arr.reduce((acc, val) => {
         if (!acc.has(val)) acc.set(val, 0)
         acc.set(val, acc.get(val) + 1)
         return acc
@@ -35,7 +34,9 @@ function calcMeanMedianMode (arr, n = arr.length) {
 
     const mean = arr.reduce((acc, val) => acc + val, 0) / n
     const median = (arr[Math.floor(n / 2)] + arr[Math.ceil((n / 2) - 1)]) / 2
-    const mode = [...count.entries()].reduce((acc, val) => (val[1] > acc[1] ? val : acc), [0, 0])[0]
+    const mode = [...frequencyMap.entries()].reduce((acc, val) => (
+        val[1] > acc[1] ? val : acc
+    ), [0, 0])[0]
 
     return [mean, median, mode]
 }
