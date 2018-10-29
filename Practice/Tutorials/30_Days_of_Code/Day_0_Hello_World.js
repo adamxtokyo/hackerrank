52d67070d8327cb78daabf64c0cde63c2ff12434
adamxtokyo <adam.grawender@gmail.com>
2018-10-29 13:57:13 +0100

    Practice > Tutorials > 30 Days of Code > Day 0: Hello, World.

---
+++ Practice/Tutorials/30_Days_of_Code/Day_0_Hello_World.js
@@ -0,0 +1,10 @@
+
+/*
+ * Practice > Tutorials > 30 Days of Code > Day 0: Hello, World.
+ * https://www.hackerrank.com/challenges/30-hello-world/problem
+ */
+
+function processData (str) {
+    console.log('Hello, World.')
+    console.log(str)
+}
