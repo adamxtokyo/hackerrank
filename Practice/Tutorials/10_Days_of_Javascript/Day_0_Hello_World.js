a9b09fc32a0418a6d126ca97c6d539613ce2dca2
adamxtokyo <adam.grawender@gmail.com>
2018-10-29 13:36:29 +0100

    Practice > Tutorials > 10 Days of Javascript > Day 0: Hello, World

---
+++ Practice/Tutorials/10_Days_of_Javascript/Day_0_Hello_World.js
@@ -0,0 +1,10 @@
+
+/*
+ * Practice > Tutorials > 10 Days of Javascript > Day 0: Hello, World!
+ * https://www.hackerrank.com/challenges/js10-hello-world/problem
+ */
+
+function greeting (str) {
+    console.log('Hello, World!')
+    console.log(str)
+}
